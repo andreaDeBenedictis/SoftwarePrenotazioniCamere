3aebfe0efa4e2dc62a506c8056e76438b3c94639
ArcangeliSammy <samuele.arcangeli@studenti.ittsrimini.edu.it>
2022-03-12 12:42:04 +0100

    Sviluppo FormDettagliCamera

---
+++ SoftwarePrenotazioniCamere/SoftwarePrenotazioniCamere/FormDettagliCamera.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace SoftwarePrenotazioniCamere
+{
+    partial class FormDettagliCamera
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPrenota = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPiano = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblTipologia = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblDescrizione = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1070, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "INDIETRO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPrenota
+            // 
+            this.btnPrenota.Location = new System.Drawing.Point(939, 31);
+            this.btnPrenota.Name = "btnPrenota";
+            this.btnPrenota.Size = new System.Drawing.Size(126, 46);
+            this.btnPrenota.TabIndex = 1;
+            this.btnPrenota.Text = "PRENOTA";
+            this.btnPrenota.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(487, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 48);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dettagli Camera";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(68, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Numero:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(68, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Piano:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(68, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 32);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Tipologia:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.lblDescrizione);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.lblTipologia);
+            this.panel1.Controls.Add(this.lblNumero);
+            this.panel1.Controls.Add(this.lblPiano);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(60, 106);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(512, 518);
+            this.panel1.TabIndex = 6;
+            // 
+            // lblPiano
+            // 
+            this.lblPiano.AutoSize = true;
+            this.lblPiano.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPiano.Location = new System.Drawing.Point(239, 57);
+            this.lblPiano.Name = "lblPiano";
+            this.lblPiano.Size = new System.Drawing.Size(99, 32);
+            this.lblPiano.TabIndex = 6;
+            this.lblPiano.Text = "lblPiano";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNumero.Location = new System.Drawing.Point(239, 137);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(128, 32);
+            this.lblNumero.TabIndex = 7;
+            this.lblNumero.Text = "lblNumero";
+            // 
+            // lblTipologia
+            // 
+            this.lblTipologia.AutoSize = true;
+            this.lblTipologia.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTipologia.Location = new System.Drawing.Point(239, 220);
+            this.lblTipologia.Name = "lblTipologia";
+            this.lblTipologia.Size = new System.Drawing.Size(139, 32);
+            this.lblTipologia.TabIndex = 8;
+            this.lblTipologia.Text = "lblTipologia";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(68, 319);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(142, 32);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Descrizione:";
+            // 
+            // lblDescrizione
+            // 
+            this.lblDescrizione.AutoSize = true;
+            this.lblDescrizione.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDescrizione.Location = new System.Drawing.Point(245, 319);
+            this.lblDescrizione.Name = "lblDescrizione";
+            this.lblDescrizione.Size = new System.Drawing.Size(163, 32);
+            this.lblDescrizione.TabIndex = 10;
+            this.lblDescrizione.Text = "lblDescrizione";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Location = new System.Drawing.Point(692, 106);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(444, 518);
+            this.panel2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(239, 293);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 32);
+            this.label5.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(33, 57);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(204, 32);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Servizi Aggiuntivi:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(24, 137);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(173, 32);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Pernottamento";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(33, 211);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(78, 32);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Piano:";
+            // 
+            // FormDettagliCamera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1208, 658);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPrenota);
+            this.Name = "FormDettagliCamera";
+            this.Text = "FormDettagliCamera";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button btnPrenota;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Panel panel1;
+        private Label label5;
+        private Label lblDescrizione;
+        private Label label8;
+        private Label lblTipologia;
+        private Label lblNumero;
+        private Label lblPiano;
+        private Panel panel2;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+    }
+}
